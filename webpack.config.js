c3694f86a21bb6e461c94c03f231903507c6beea
Jordan Scholten <schojo13@gmail.com>
2018-10-19 14:11:48 -0700

    Setup Babel, React, Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,39 @@
 module.exports = options => {
   return {
+    watch: true,
     entry: './index.js',
     output: {
+      path: '/public/',
       filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                cacheDirectory: true,
+                presets: [
+                  'babel-preset-env',
+                  'babel-preset-react',
+                  'babel-preset-stage-1'
+                ]
+              }
+            },
+          ]
+        },
+        {
+          test: /.css$/,
+          exclude: /node_modules/,
+          use: [
+            { loader: 'style-loader' },
+            { loader: 'css-loader' }
+          ]
+        }
+      ]
     }
   }
 }
