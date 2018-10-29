2a046634fcaadf6cbe318c7e07fa722e2d2a6626
Jordan Scholten <schojo13@gmail.com>
2018-10-29 11:03:19 -0700

    Set up basic routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
-import Header from './Header.js';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import Colors from './Colors.js';
+// import Header from './Header.js';
 import './App.css';
+import './Colors.css';
+import './Header.css';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="main">
-        <Header />
-        <div className="main-content">
-          <p className="title">Red Safety Main Content</p>
+      <Router>
+        <div className="main">
+         {/*<Header />*/}
+         <div className="header">
+           <div>
+             <img src="https://www.redsafety.com/wp-content/uploads/2017/02/logo.png" />
+           </div>
+           <div className="navigation">
+             <Link to="/">Home</Link>
+             <Link to="/colors">Products</Link>
+             About People Careers
+           </div>
+         </div>
+         <Route exact path="/" component={()=>{
+             return (
+               <div className="main-content">
+                 <p className="title">Red Safety Main Content</p>
+                 <div className="three">
+                   Number Three
+                   <div className="connector"></div>
+                 </div>
+                 <div className="four">Number Four</div>
+               </div>
+             )
+           }}/>
+          <Route exact path="/colors" component={Colors}/>
+          <div className="footer">Footer</div>
         </div>
-        <div className="three">
-          Number Three
-          <div className="connector"></div>
-        </div>
-        <div className="two">Number Two</div>
-      </div>
+      </Router>
     )
   }
 }
--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Link from 'react-router-dom';
 import './Header.css';
 
 export default class Header extends Component {
@@ -8,7 +9,10 @@ export default class Header extends Component {
         <div>
           <img src="https://www.redsafety.com/wp-content/uploads/2017/02/logo.png" />
         </div>
-        <div className="navigation">Products About People Careers</div>
+        <div className="navigation">
+          <Link to="/colors">Products</Link>
+          About People Careers
+        </div>
       </div>
     )
   }
