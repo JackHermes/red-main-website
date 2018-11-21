402ac5e68c514a09c6c5cd41730922915972403e
Jordan Scholten <schojo13@gmail.com>
2018-11-21 15:24:09 -0800

    Add breathing button-bullet point

--- src/Connector.js
+++ src/Connector.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
+// import logo from './img/grey-logo.png';
+
 
 const Connector = () => {
   return (
     <div className='connector-container'>
       <div className='connector-line'>
         <div className='connector-bullet'></div>
+        <div className='connector-bullet-pulse'></div>
+        <div className='connector-bullet-background'></div>
       </div>
     </div>
   )
