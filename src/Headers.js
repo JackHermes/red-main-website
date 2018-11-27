3992fb9ae650d739eee7b3edba0b4b8d557e9aa2
Jordan Scholten <schojo13@gmail.com>
2018-11-27 15:18:25 -0800

    Swap img logo for text logo

--- src/Headers.js
+++ src/Headers.js
@@ -5,10 +5,14 @@ import MobileMenu from './MobileMenu.js';
 
 import './Headers.css';
 
+// <div><span className='font-test left-logo'>red</span><span className='font-test right-logo'>safety</span></div>
+// <img alt='Red Safety logo' src='https://www.redsafety.com/wp-content/uploads/2017/02/logo.png' />
+// <div className='logo'></div>
+
 const SmallScreenHeader = () => {
   return (
     <div className='header-sm'>
-      <Link to='/'><div className='logo'></div></Link>
+      <Link to='/'><div className='font-test'><span className='left-logo'>red</span><span className='right-logo'>safety</span></div></Link>
       <MobileMenu />
       <div className='contact-us-box'>(###)###-####</div>
     </div>
@@ -18,7 +22,7 @@ const SmallScreenHeader = () => {
 const LargeScreenHeader = () => {
   return (
     <div className='header-lg'>
-      <Link to='/'><img alt='Red Safety logo' src='https://www.redsafety.com/wp-content/uploads/2017/02/logo.png' /></Link>
+      <Link to='/'><span className='font-test left-logo'>red</span><span className='font-test right-logo'>safety</span></Link>
       <div className='navigation'>
         <Link to='/products'>Products</Link>
         <Link to='/services'>Services</Link>
