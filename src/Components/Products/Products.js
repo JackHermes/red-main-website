b417b16fe5c1b4b319473a22c487c4fa9830cd6b
Jordan Scholten <schojo13@gmail.com>
2018-12-28 10:48:14 -0800

    Add some mock 'Products' page content

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
-const Products = () => {
+import { ColorBoxes, SectionProtect, SectionSafety, SectionProducts, SectionTestimonials } from '../Common/Sections.js';
+
+
+import './Products.css';
+
+export default class Products extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
     return (
       <div className='main-content' >
-        <div>'Products' Here</div>
+        <SectionTestimonials />
+        <ColorBoxes />
       </div>
     );
-  };
-
-export default Products;
+  }
+};
