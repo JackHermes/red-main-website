3acd6f14004c12ee93226e60252d7f06769a7f64
Jordan Scholten <schojo13@gmail.com>
2019-02-05 14:17:32 -0800

    Improve header style and condense its stylesheets

--- src/Components/Headers/Headers.js
+++ src/Components/Headers/Headers.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 
 import MobileMenu from './MobileMenu.js';
 import './Headers.css';
-import './Headers-sm.css';
 import './Headers-med.css';
 import './Headers-lg.css';
 
@@ -11,10 +10,10 @@ import logo from '../../img/approved-logo.jpg';
 
 const SmallScreenHeader = () => {
   return (
-    <div className='header-sm'>
-      <Link to='/'><img className='logo-sm' src={logo} /></Link>
+    <div className='header'>
+      <Link to='/'><img className='logo' src={logo} /></Link>
       <MobileMenu />
-      <div className='contact-us-box contact-us-box-sm'>(###)###-####</div>
+      <div className='contact-us-box'>(888)557-5167</div>
     </div>
   )
 }
@@ -29,7 +28,7 @@ const LargeScreenHeader = () => {
         <Link to='/about' className='nav-about'>What are We About?</Link>
         <Link to='/colors' className='nav-colors'>Colors</Link>
       </div>
-      <div className='contact-us-box'>(###)###-####</div>
+      <div className='contact-us-box contact-us-box-lg'>(888)557-5167</div>
     </div>
   )
 }
